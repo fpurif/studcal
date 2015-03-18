40f97715b8ac59f36ce7203647d3361585cc4f3f
fpurif <franchesca.jp@mail.com>
2015-03-18 04:03:07 -0600

    add event; dropdown view edits

--- Calendar/app/src/main/java/com/paulinefran/calendar/CustomOnItemSelectedListener.java
+++ Calendar/app/src/main/java/com/paulinefran/calendar/CustomOnItemSelectedListener.java
@@ -10,10 +10,31 @@ public class CustomOnItemSelectedListener implements OnItemSelectedListener{
 	// CALL OTHER ACTIVITIES FOR DIFF VIEWS HERE
 	public void onItemSelected(AdapterView<?> parent, View view, int pos,
             long id) {
-         
-        Toast.makeText(parent.getContext(), 
+
+        /*Toast.makeText(parent.getContext(),
                 "On Item Select : \n" + parent.getItemAtPosition(pos).toString(),
-                Toast.LENGTH_LONG).show();
+                Toast.LENGTH_LONG).show(); */
+
+        if (new String("By Day").equals(parent.getItemAtPosition(pos).toString())) {
+            //Insert Daily view here
+            Toast.makeText(parent.getContext(),
+                    "By Day TEST",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if (new String("By Week").equals(parent.getItemAtPosition(pos).toString())) {
+            //Weekly view
+                Toast.makeText(parent.getContext(),
+                        "By Week TEST",
+                        Toast.LENGTH_LONG).show();
+            }
+        else{
+            //Monthly view
+                Toast.makeText(parent.getContext(),
+                        "By Month TEST",
+                        Toast.LENGTH_LONG).show();
+                }
+
+
     }
  
     @Override
--- Calendar/app/src/main/java/com/paulinefran/calendar/MainActivity.java
+++ Calendar/app/src/main/java/com/paulinefran/calendar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.paulinefran.calendar;
 
 
+import android.provider.CalendarContract;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.*;
 import android.view.Menu;
@@ -17,6 +18,9 @@ import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+
+import java.util.GregorianCalendar;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -46,7 +50,24 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+            //ADD EVENT
+            Toast.makeText(getApplicationContext(),
+                    "ADD EVENT TEST",
+                    Toast.LENGTH_LONG).show();
+            Intent calIntent = new Intent(Intent.ACTION_INSERT);
+            calIntent.setType("vnd.android.cursor.item/event");
+            calIntent.putExtra(CalendarContract.Events.TITLE, "Title Example");
+            calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Location Example");
+            calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "Description Example");
 
+            GregorianCalendar calDate = new GregorianCalendar(2015, 3, 26);
+            calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+            calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                    calDate.getTimeInMillis());
+            calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                    calDate.getTimeInMillis());
+
+            startActivity(calIntent);
 			return true;
 		}
 		else if(id == R.id.semOption){
@@ -73,15 +94,31 @@ public class MainActivity extends ActionBarActivity {
 		
 	};*/
 	
-	
+	/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	Following code for onlongclick on month doesn't work, see above for alternative
+	 */
 	public void initializeCalendar() {
-		calendar = (CalendarView) findViewById(R.id.calendarmonth);
-		//calendar.setOnLongClickListener(dateListener);
-		
-		
-			
-		};
+		CalendarView calendarmonth = (CalendarView) findViewById(R.id.calendarmonth);
+
+        //press and hold listener in month
+        calendarmonth.setOnLongClickListener(
+                new CalendarView.OnLongClickListener(){
+                    public boolean OnLongClick(View CalendarView){
+                        Toast.makeText(getApplicationContext(),
+                                "HI",
+                                Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onLongClick(View CalendarView) {
+                        return false;
+                    }
+                }
+        );
+    }
+
 		
 
-			}
+}
 	
